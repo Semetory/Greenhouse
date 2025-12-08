155e4632a5f3fe852678fac4b3ae07c6f5f8c2e6
Dmitriy Semenov <45949611+Semetory@users.noreply.github.com>
2025-12-08 12:10:56 +0300

    Update Subject.java

--- Observer/src/main/java/com/example/observer/observer/Subject.java
+++ Observer/src/main/java/com/example/observer/observer/Subject.java
@@ -3,9 +3,9 @@ package com.example.observer.observer;
 import java.util.List;
 
 public interface Subject {
-    void notifyAllObservers(); // уведомить всех наблюдателей
-    void attach(Observer obs); // добавить наблюдателя
-    void detach(Observer obs); // удалить наблюдателя
-    int getState(); // получить текущее состояние
-    void setState(int time); // установить состояние
+    void notifyAllObservers(); // увед all набл
+    void attach(Observer obs); // + набл
+    void detach(Observer obs); // - набл
+    int getState();
+    void setState(int time);
 }
